785486b7f9e46d4a1cf87b9836dbae9c8ed0724e
Brpadilha <brpadilha.dev@gmail.com>
2019-08-15 18:03:56 -0300

    Finaliza o backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,9 +4,7 @@ const DevController = require("./controllers/DevController");
 const LikeController = require("./controllers/LikeController");
 const DisLikeController = require("./controllers/DislikeController");
 
-routes.get("/", (req, res) => {
-  return res.send({ message: `Olá ${req.query.name}` });
-});
+routes.get("/devs", DevController.index);
 
 routes.post("/devs", DevController.store);
 routes.post("/devs/:devId/likes", LikeController.store);
