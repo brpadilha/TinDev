7c1c73787c17bd186a6bafb012ca802e82be98b1
Brpadilha <brpadilha.dev@gmail.com>
2019-08-15 17:35:16 -0300

    Like, dislike e match construidos

--- backend/src/controllers/DislikeController.js
+++ backend/src/controllers/DislikeController.js
@@ -12,11 +12,7 @@ module.exports = {
       return res.status(400).json({ error: "Dev not exists" });
     } //tem algo na sua requisiçõa errada
 
-    if (targetDev.likes.includes(loggedDev._id)) {
-      console.log("Deu Match");
-    }
-
-    loggedDev.likes.push(targetDev._id);
+    loggedDev.dislikes.push(targetDev._id);
 
     await loggedDev.save();
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const routes = express.Router();
 const DevController = require("./controllers/DevController");
+const LikeController = require("./controllers/LikeController");
+const DisLikeController = require("./controllers/DislikeController");
 
 routes.get("/", (req, res) => {
   return res.send({ message: `Olá ${req.query.name}` });
 });
 
 routes.post("/devs", DevController.store);
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DisLikeController.store);
 
 module.exports = routes;
