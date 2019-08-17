ffed1f82f7a8a7bda6eabb0907f809c0e50d4a2b
Brpadilha <brpadilha.dev@gmail.com>
2019-08-17 13:38:06 -0300

    termina de colocar as imagens no main

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,83 @@
 import React from 'react';
+import './Main.css';
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
 
-export default function Main(){
-  return <h1>Hello World</h1>
+export default function Main({ match }){
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev"/>
+      <ul>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/37818334?v=4" alt=""/>
+          <footer>
+            <strong>
+              Bruno Padilha
+            </strong>
+            <p>
+              Mechanical Engineer that loves to code, now I am studying for to be a software engineer.
+            </p>
+          </footer>
+          <div className="buttons">
+            <button type="button" >
+              <img src={dislike} alt="Dislike"/>
+              <img src={like} alt="like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/37818334?v=4" alt="" />
+          <footer>
+            <strong>
+              Bruno Padilha
+            </strong>
+            <p>
+              Mechanical Engineer that loves to code, now I am studying for to be a software engineer.
+            </p>
+          </footer>
+          <div className="buttons">
+            <button type="button" >
+              <img src={dislike} alt="Dislike" />
+              <img src={like} alt="like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/37818334?v=4" alt="" />
+          <footer>
+            <strong>
+              Bruno Padilha
+            </strong>
+            <p>
+              Mechanical Engineer that loves to code, now I am studying for to be a software engineer.Mechanical Engineer that loves to code, now I am studying for to be a software engineer.Mechanical Engineer that loves to code, now I am studying for to be a software engineer.
+            </p>
+          </footer>
+          <div className="buttons">
+            <button type="button" >
+              <img src={dislike} alt="Dislike" />
+              <img src={like} alt="like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/37818334?v=4" alt="" />
+          <footer>
+            <strong>
+              Bruno Padilha
+            </strong>
+            <p>
+              Mechanical Engineer that loves to code, now I am studying for to be a software engineer.
+            </p>
+          </footer>
+          <div className="buttons">
+            <button type="button" >
+              <img src={dislike} alt="Dislike" />
+              <img src={like} alt="like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
 }
